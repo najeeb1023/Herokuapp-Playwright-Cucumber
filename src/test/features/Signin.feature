1d4df354d05c06bb274fbae13d8de7f41ceafc4e
AliRaza <ali.raza@etaplus.energy>
2023-11-10 23:48:11 +0100

    Initial test case to find function added.

--- src/test/features/Signin.feature
+++ src/test/features/Signin.feature
@@ -1,7 +1,8 @@
 Feature: Tool Shop 
 
 Background: Landing Page 
-  Given User is able to click a sigin 
+  Given User visits the website 
 
 Scenario: User is able to enter a correct credentials 
-  When 
+  When User goes through the web functions
+  Then User is able to select one
--- src/test/pages/LandingPage.ts
+++ src/test/pages/LandingPage.ts
@@ -1,10 +1,27 @@
-import { Page } from "@playwright/test";
+import { expect, Page } from "@playwright/test";
+import { pageFixture } from "../hooks/pageFixture";
+import { PageElement } from "../resources/Interfaces/iPagesElelment";
+import * as landingPageLocators from "../resources/landingPage.json"
 
+function getResource(resourceName :string ){
+    return landingPageLocators.webElements.find((element: PageElement) => element.elementName == resourceName) as PageElement
+}
 export class LandingPage{
-
+    landingPageLocators = {
+        functionList:() => pageFixture.page.locator(getResource('allFunctionList').selectorValue),
+    }
+    public async goToLandingPage():Promise<any>{
+        await pageFixture.page.goto('https://the-internet.herokuapp.com/')
+    }
+    public async checkSelectedFunction():Promise<any>{
+        const numberOfFunctions = await this.landingPageLocators.functionList().count();
+        for(let i=1;i<=numberOfFunctions;i++){
+            const el = await pageFixture.page.locator(getResource('getFunction').selectorValue.replace('placeholder', i.toString()));
+            const arr = [el.getByText('Broken Images').click()];
+            console.log(arr);
+        }
+    }
     constructor(public page: Page){
         this.page = page;
     }
-
-
 }
