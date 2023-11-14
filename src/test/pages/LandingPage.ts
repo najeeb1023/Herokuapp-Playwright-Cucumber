229151b8ba067b0000b6431514bd7ef4b8099d6c
Gul Najeeb Channa <najeeb.capricorn@gmail.com>
2023-11-14 23:09:18 +0100

    Nested Frames, WIP.

--- src/test/pages/LandingPage.ts
+++ src/test/pages/LandingPage.ts
@@ -1,18 +1,20 @@
 import { expect, Page } from "@playwright/test";
 import { pageFixture } from "../hooks/pageFixture";
-import { PageElement } from "../resources/Interfaces/iPagesElelment";
+import { PageElement } from "../resources/Interfaces/iPagesElement";
 import * as landingPageLocators from "../resources/landingPage.json"
 
-function getResource(resourceName :string ){
+function getResource(resourceName: string ){
     return landingPageLocators.webElements.find((element: PageElement) => element.elementName == resourceName) as PageElement
 }
 export class LandingPage{
     landingPageLocators = {
         functionList:() => pageFixture.page.locator(getResource('allFunctionList').selectorValue),
     }
+
     public async goToLandingPage():Promise<any>{
-        await pageFixture.page.goto('https://the-internet.herokuapp.com/')
+        await pageFixture.page.goto('https://the-internet.herokuapp.com/');
     }
+
     public async checkSelectedFunction(getFunction):Promise<any>{
         await pageFixture.page.locator("//div[@id='content']//ul//li[contains(., '"+getFunction+"')]/a").click();
     }
@@ -23,11 +25,8 @@ export class LandingPage{
         for(let i=1;i<=dataElements;i++){
         const getLastName = await pageFixture.page.locator("//table[@id='table2']//tbody//tr[placeholder]//td[3]".replace('placeholder', i.toString()));
         console.log(await getLastName.textContent());
-
         }
-
     }
-
     constructor(public page: Page){
         this.page = page;
     }
