985faddde211922ae26e101cc3059e3038ee2867
Gul Najeeb Channa <najeeb.capricorn@gmail.com>
2023-11-19 18:02:52 +0100

    Redirection functionality WIP.

--- src/test/pages/RedirectPage.ts
+++ src/test/pages/RedirectPage.ts
@@ -1,10 +1,14 @@
-import { Page } from "@playwright/test";
+import { Page, expect } from "@playwright/test";
 import { pageFixture } from "../hooks/pageFixture";
 
 export class Redirect {
 
     public async goToFunctionRedirect(getFunction: string):Promise<void>{
         await pageFixture.page.locator("//div[@id='content']//ul//li[contains(., '"+getFunction+"')]/a").click();
+    }
+
+    public async assertRedirection():Promise<void>{
+        await expect(pageFixture.page).toHaveURL('https://the-internet.herokuapp.com/redirector')
     }
 
 
